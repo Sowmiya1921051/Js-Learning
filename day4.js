5ee096dcdaababf8c1c23140012a99ab88c2cd97
sowmiya <sowmiyanallasivam3434@gmail.com>
2023-09-23 21:20:57 +0530

    first commit

---
+++ day4.js
@@ -0,0 +1,24 @@
+// const fruits=["apple","mango","papaya","orange"]// 
+// fruits.push("banana")
+// console.log(fruits)
+
+
+// let obj={
+//     name:"Sowmi",
+//     age:21
+// }
+// console.log("Name :"+obj.name+" Age :"+obj.age)
+
+
+// function fun()
+// {
+//     let i=1,sum=0;
+//     while(i<=100)
+//     {
+//         sum=sum+i;  
+//         i++; 
+//     }
+  
+//     console.log(sum)
+// }
+// fun()
