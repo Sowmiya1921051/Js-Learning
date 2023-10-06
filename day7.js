27052df7a99bc4b6eee05c74491a9b3a68c7101c
sowmiya <sowmiyanallasivam3434@gmail.com>
2023-10-06 12:24:21 +0530

    template literals

--- day7.js
+++ day7.js
@@ -2,17 +2,28 @@
 // "remembers" and can still access variables from the scope in which it was created,
 // even after that scope has finished executing. Closures are useful for encapsulating data and behavior and are a key feature in JavaScript for maintaining state and creating private variables.
 
-function outer()
-{
-   const a="hello 1"
-   console.log(a)
-    function inner()
-    {
-        const b="hello 2";
-        console.log(b)
+// function outer()
+// {
+//    const a="hello 1"
+//    console.log(a)
+//     function inner()
+//     {
+//         const b="hello 2";
+//         console.log(b)
         
-    }
-    return inner
+//     }
+//     return inner
+// }
+// let execute = outer()
+// execute()
+
+//Template literals
+let name="Sowmiya"
+let regno=51
+function myFunction() {
+  var localVar = `My name is ${name} and my Regno ${regno}` ;
+  console.log(localVar);
 }
-let execute = outer()
-execute()
+
+myFunction(); 
+
