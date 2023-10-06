2c388ea7eb76f3b719cfee7d614dff5e867ac99b
sowmiya <sowmiyanallasivam3434@gmail.com>
2023-10-06 12:18:41 +0530

    Click to change

---
+++ day7.js
@@ -0,0 +1,18 @@
+//closure a closure in JavaScript is a function that
+// "remembers" and can still access variables from the scope in which it was created,
+// even after that scope has finished executing. Closures are useful for encapsulating data and behavior and are a key feature in JavaScript for maintaining state and creating private variables.
+
+function outer()
+{
+   const a="hello 1"
+   console.log(a)
+    function inner()
+    {
+        const b="hello 2";
+        console.log(b)
+        
+    }
+    return inner
+}
+let execute = outer()
+execute()
