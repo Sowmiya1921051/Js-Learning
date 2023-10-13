a77063db543fd4b21a98b30f909d45775ea3140d
sowmiya <sowmiyanallasivam3434@gmail.com>
2023-10-13 17:22:55 +0530

    Calculator

--- Problems/Calculator.js
+++ Problems/Calculator.js
@@ -3,11 +3,12 @@ const prompt=ps()
 let details = "Select operation: \n 1.Addition \n 2.Subtraction \n 3.Multiplication \n 4.Division"
 console.log(details)
 let Choose=prompt("Enter choice (1/2/3/4): ")
-const a=prompt("Enter first number: ")
-const b=prompt("Enter second number ")
+const a = parseFloat(prompt("Enter first number: "));
+const b = parseFloat(prompt("Enter second number: ")); 
 if(Choose == 1)
 {
     console.log(a ," + ", b ," = ",a+b)
+  
 }
 else if(Choose == 2)
 {
