deba9943fbe7dc94945617c04240a673193c7958
sowmiya <sowmiyanallasivam3434@gmail.com>
2023-10-13 17:12:31 +0530

    VowelsandConst

--- Problems/VowelsandConst.js
+++ Problems/VowelsandConst.js
@@ -1,11 +1,11 @@
-const name="Sowmiya"
+const name="SowmiyA"
 let vowels=0
 let consonant=0
 function count()
 {
     for(let i=0;i<name.length;i++)
     {
-        if(name[i]=='a' || name[i]=='e' || name[i]=='i' || name[i]=='o' || name[i]=='u')
+        if(name[i]=='a' || name[i]=='e' || name[i]=='i' || name[i]=='o' || name[i]=='u' || name[i]=='A' || name[i]=='E' || name[i]=='I' || name[i]=='O'|| name[i]=='U' )
         {
             vowels++
         }
